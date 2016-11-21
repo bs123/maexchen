e61e163c5d9827a3db33aea876c68d312a5f6440
Tobias Tacke <ttacke@ttacke-mobile>
2016-11-21 20:59:53 +0100

    working version

--- index2.js
+++ index2.js
@@ -7,7 +7,8 @@ var dgram = require('dgram');
 
 
 var client = dgram.createSocket('udp4');
-
+var ourDiceRoll = [0,0];
+var opponentDiceRoll = [0,0];
 
 client.on('message', (msg, rinfo) => {
 	var param = msg.toString().split(';');
@@ -15,6 +16,20 @@ client.on('message', (msg, rinfo) => {
 	if(param[0] == 'ROUND STARTING') {
 		answer('JOIN', param[1]);
 	} else if(param[0] == 'YOUR TURN') {
+		answer('ROLL', param[1]);
+	} else if(param[0] == 'ROLLED' ) {
+		console.log('+++++ROLLED' + param[1]);
+		
+		ourDiceRoll = param[1].split(',');
+		console.log('+++++ROLLED' + ourDiceRoll[0] + ',' + ourDiceRoll[1] );
+		answer('ANNOUNCE', ourDiceRoll.join(',')+ ';'+param[2])
+		 	
+	} else if(param[0] == 'ANNOUNCED' ) {
+		console.log('++ANNOUNCED' + param[1]);
+		//answer('ROLL', param[1]);	
+		opponentDiceRoll = param[1].split(',');
+		console.log('+++++OPPONEND AN' + opponentDiceRoll[0] + ',' + opponentDiceRoll[1] );
+		
 	}
 	console.log(param);
 });
