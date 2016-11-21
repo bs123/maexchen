243b20b3a36a75cc42006497ca90f078d579de44
Tobias Tacke <ttacke@ttacke-mobile>
2016-11-21 20:27:21 +0100

    ii

--- index2.js
+++ index2.js
@@ -1,12 +1,23 @@
 var PORT = 9000;
-var HOST = '255.255.255.0';
+var HOST = '10.10.10.1';
 
 var dgram = require('dgram');
-var message = new Buffer('My KungFu is Good!');
+
+
+
 
 var client = dgram.createSocket('udp4');
+
+
+client.on('message', (msg, rinfo) => {
+	
+	console.log(msg);
+	console.log(message);
+});
+
+var message = new Buffer('REGISTER;JT');
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
     if (err) throw err;
-    console.log('UDP message sent to ' + HOST +':'+ PORT);
-    client.close();
+    console.log('Start');
+//    client.close();
 });
